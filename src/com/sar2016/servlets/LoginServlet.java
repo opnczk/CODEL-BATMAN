5218fdf3d58b0c08b92915d55a2d23c61157d4ba
Olivier Panczuk <opanczuk@gmail.com>
2016-10-23 18:09:27 +0200

    Added Google People Api libs and stub code for back-end auth

--- src/com/sar2016/servlets/LoginServlet.java
+++ src/com/sar2016/servlets/LoginServlet.java
@@ -1,6 +1,10 @@
 package com.sar2016.servlets;
 
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -9,6 +13,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleBrowserClientRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+import com.google.api.services.people.v1.People;
 import com.sar2016.entities.Contact;
 import com.sar2016.entities.Enterprise;
 import com.sar2016.services.ContactService;
@@ -72,4 +85,50 @@ public class LoginServlet extends HttpServlet {
 		}
 	}
 
+	 public void setUp() throws IOException {
+		    HttpTransport httpTransport = new NetHttpTransport();
+		    JacksonFactory jsonFactory = new JacksonFactory();
+
+		    // Go to the Google API Console, open your application's
+		    // credentials page, and copy the client ID and client secret.
+		    // Then paste them into the following code.
+		    String clientId = "128124732452-h8ja44bqta91s95ui0empsgde5nj122i.apps.googleusercontent.com";
+		    String clientSecret = "6qlRPKgljuED0dI2XglPcMAs";
+
+		    // Or your redirect URL for web based applications.
+		    String redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
+		    String scope = "https://www.googleapis.com/auth/contacts.readonly";
+
+		    // Step 1: Authorize -->
+		    String authorizationUrl = new GoogleBrowserClientRequestUrl(clientId,
+		                                                                redirectUrl,
+		                                                                Arrays.asList(scope))
+		        .build();
+
+		    // Point or redirect your user to the authorizationUrl.
+		    System.out.println("Go to the following link in your browser:");
+		    System.out.println(authorizationUrl);
+
+		    // Read the authorization code from the standard input stream.
+		    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		    System.out.println("What is the authorization code?");
+		    String code = in.readLine();
+		    // End of Step 1 <--
+
+		    // Step 2: Exchange -->
+		    GoogleTokenResponse tokenResponse = new GoogleAuthorizationCodeTokenRequest(
+		        httpTransport, jsonFactory, clientId, clientSecret, code, redirectUrl).execute();
+		    // End of Step 2 <--
+
+		    GoogleCredential credential = new GoogleCredential.Builder()
+		        .setTransport(httpTransport)
+		        .setJsonFactory(jsonFactory)
+		        .setClientSecrets(clientId, clientSecret)
+		        .build()
+		        .setFromTokenResponse(tokenResponse);
+
+		    People peopleService = new People.Builder(httpTransport, jsonFactory, credential)
+		        .build();
+		  }
+	
 }
